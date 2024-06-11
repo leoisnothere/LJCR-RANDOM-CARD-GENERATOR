d37ad691b7e7a2bfea88683a7c1b39053d4ec017
leoisnothere <82447759+leoisnothere@users.noreply.github.com>
2024-06-11 16:11:33 +0000

    Update.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
-/* eslint-disable */
+//include bootstrap npm library into the bundle
 import "bootstrap";
+//include your own styles
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  window.genCard = function() {
+    var suit = ["♥", "♠", "♣", "♦"];
+    var numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+
+    function randomCard() {
+      var randomSuit = suit[Math.floor(Math.random() * suit.length)];
+      var randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+      document.querySelector(".icon").innerHTML = randomSuit;
+      document.querySelector(".cardContentContainer").innerHTML = randomNumber;
+      document.querySelector(".iconTwo").innerHTML = randomSuit;
+
+      if (randomSuit == "♥" || randomSuit == "♦") {
+        document.querySelector("#maincardcontainer").style.color = "red";
+      } else if (randomSuit == "♠" || randomSuit == "♣") {
+        document.querySelector("#maincardcontainer").style.color = "black";
+      }
+    }
+
+    randomCard();
+  };
+
+  window.genCard();
 };
